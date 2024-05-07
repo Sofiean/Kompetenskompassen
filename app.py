448ee902b0bd9f1eac1aea8f9b7332f0c59f6297
Sofiean <gusandsodm@student.gu.se>
2024-05-07 14:44:26 +0200

    knappar
    knappar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 
 app = Flask(__name__)
@@ -23,23 +23,40 @@ def index():
                                          municipality_options=all_municipalities,
                                          years_options=all_years)
 
-@app.route('/analysis', methods=['POST'])
+@app.route('/analysis', methods=['POST', 'GET'])
 def analysis():
-    occupation = request.form['occupation']
-    municipality = request.form['municipality']
-    year = request.form['years']
-    
-    # Filter data based on user input
-    filtered_data = csv_files[year][(csv_files[year]['Occupation Field'] == occupation) & 
-                                     (csv_files[year]['Municipality'] == municipality)]
-    
-    # Get top 10 skills
-    top_skills = filtered_data.groupby('Skill').sum()['Count'].nlargest(10)
-    
-    # Convert top_skills to list of tuples for passing to template
-    top_skills_list = list(top_skills.items())
-    
-    return render_template('index.html', top_skills=top_skills_list)
+    if request.method == 'POST':
+        occupation = request.form['occupation']
+        municipality = request.form['municipality']
+        year = request.form['years']
+        
+        # Filter data based on user input
+        filtered_data = csv_files[year][(csv_files[year]['Occupation Field'] == occupation) & 
+                                         (csv_files[year]['Municipality'] == municipality)]
+        
+        # Get top 10 skills
+        top_skills = filtered_data.groupby('Skill').sum()['Count'].nlargest(10)
+        
+        # Convert top_skills to list of tuples for passing to template
+        top_skills_list = list(top_skills.items())
+        
+        return render_template('index.html', top_skills=top_skills_list,
+                                             occupation_options=all_occupations,
+                                             municipality_options=all_municipalities,
+                                             years_options=all_years)
+    else:
+        return render_template('index.html', occupation_options=all_occupations,
+                                             municipality_options=all_municipalities,
+                                             years_options=all_years)
+
+@app.route('/index1')
+def index1():
+    return render_template('index1.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+#hej
+ 
